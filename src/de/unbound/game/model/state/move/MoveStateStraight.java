e7a248958882b8d77e3d1a2e84aae2dac7a91d42
Craid <michel.schmitz_1992@hotmail.com>
2016-04-12 17:11:36 +0200

    Update

--- src/de/unbound/game/model/state/move/MoveStateStraight.java
+++ src/de/unbound/game/model/state/move/MoveStateStraight.java
@@ -1,5 +1,7 @@
 package de.unbound.game.model.state.move;
 
+import com.badlogic.gdx.math.Vector2;
+
 import de.unbound.game.model.entities.mobile.MobileEntity;
 
 public class MoveStateStraight extends AbstractMoveState {
@@ -11,8 +13,10 @@ public class MoveStateStraight extends AbstractMoveState {
 	@Override
 	public void update(double deltaTime) {
 		//TODO Marwin mach mal
-//		e.setVelocity(e.getVelocity().scl((float)(1+deltaTime)));
-//		e.setPosition(e.getPosition().add(e.getVelocity()));
+		e.setVelocity(new Vector2(0.1f,0));
+		e.setDirection(e.getDirection().rotate(5));
+		//		e.setVelocity(e.getVelocity().scl((float)(1+deltaTime)));
+		e.setPosition(e.getPosition().add(e.getVelocity()));
 //		System.out.println(e.getPosition());
 	}
 }
